adde74f23782149c219c101b0090ddffb2728681
hiroki-h <hiroki-h@weseek.co.jp>
2023-03-07 19:00:43 +0900

    lstm coding

--- lstm.py
+++ lstm.py
@@ -1,23 +1,98 @@
+#
+# 必要なライブラリーの読み込み
+#
 import numpy as np
-from sklearn.metrics import r2_score
-from pandas_datareader import data as pdr
-import yfinance as yf
-yf.pdr_override()
-from datetime import datetime
+import pandas as pd
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import *
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.utils import plot_model
 from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import Dense, LSTM, Dropout
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_percentage_error
 import matplotlib.pyplot as plt
-plt.style.use("fivethirtyeight")
+plt.style.use('ggplot') #グラフスタイル
+plt.rcParams['figure.figsize'] = [12, 9] # グラフサイズ
+#
+# データセット読み込み
+#
+url = 'https://www.salesanalytics.co.jp/bgr8'
+df = pd.read_csv(url)
+#
+# 前処理
+#
+## 変換
+dataset = df.y.values #NumPy配列へ変換
+dataset = dataset.astype('float32')    #実数型へ変換
+dataset = np.reshape(dataset, (-1, 1)) #1次元配列を2次元配列へ変換
+## ラグ付きデータセット生成関数
+def gen_dataset(dataset, lag_max):
+    X, y = [], []
+    for i in range(len(dataset) - lag_max):
+        a = i + lag_max
+        X.append(dataset[i:a, 0]) #ラグ変数
+        y.append(dataset[a, 0])   #目的変数
+    return np.array(X), np.array(y)
+## 分析用データセットの生成
+lag_max = 365
+X, y = gen_dataset(dataset, lag_max)
+#
+# データ分割
+#
+test_length = 30 #テストデータの期間
+X_train_0 = X[:-test_length,:] #学習データ
+X_test_0 = X[-test_length:,:]  #テストデータ
+y_train_0 = y[:-test_length] #学習データ
+y_test_0 = y[-test_length:]  #テストデータ
+y_train = y_train_0.reshape(-1,1)
+y_test = y_test_0.reshape(-1,1)
+#
+# 正規化（0-1の範囲にスケーリング）
+#
+## 目的変数y
+scaler_y = MinMaxScaler(feature_range=(0, 1))
+y_train = scaler_y.fit_transform(y_train)
+## 説明変数X
+scaler_X = MinMaxScaler(feature_range=(0, 1))
+X_train_0 = scaler_X.fit_transform(X_train_0)
+X_test_0 = scaler_X.transform(X_test_0)
 
+# モデル構築用にデータを再構成（サンプル数、タイムステップ, 特徴量数）
+X_train = np.reshape(X_train_0, (X_train_0.shape[0],1,X_train_0.shape[1]))
+X_test = np.reshape(X_test_0, (X_test_0.shape[0],1,X_test_0.shape[1]))
+print('X_train:',X_train.shape) #確認
+print('X_test:',X_test.shape) #確認
 
-s_target = 'GOOG'
-df = pdr.get_data_yahoo(s_target, start='2014-01-01', end=datetime.now())
-df.head()
+# モデル定義
+model = Sequential()
+model.add(LSTM(300,input_shape=(X_train.shape[1], X_train.shape[2])))
+model.add(Dropout(0.2))
+model.add(Dense(1, activation='linear'))
+# コンパイル
+model.compile(loss='mean_squared_error', optimizer='adam')
+# モデルの視覚化
+plot_model(model,show_shapes=True)
 
-plt.figure(figsize=(16,6))
-plt.title(s_target + ' Close Price History')
-plt.plot(df['Close'])
-plt.xlabel('Date', fontsize=14)
-plt.ylabel('Close Price USD ($)', fontsize=14)
+# EaelyStoppingの設定
+early_stopping =  EarlyStopping(monitor='val_loss',
+                                min_delta=0.0,
+                                patience=2)
+# 学習の実行
+history = model.fit(X_train, y_train,
+                    epochs=1000,
+                    batch_size=128,
+                    validation_split=0.2,
+                    callbacks=[early_stopping] ,
+                    verbose=1, 
+                    shuffle=False)
+
+# 学習結果の出力
+model.summary()
+plt.plot(history.history['loss'], label='Train Loss')
+plt.plot(history.history['val_loss'], label='valid Loss')
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epochs')
+plt.legend(loc='upper right')
 plt.show()
